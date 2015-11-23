a96a753240208ab9da2ea1098d200c99eb71d6e4
Arnold Myint <blinkmonster@gmail.com>
2015-11-23 12:17:51 -0800

    Added some more print lines.

--- demo/src/demo/FirstApp.java
+++ demo/src/demo/FirstApp.java
@@ -4,6 +4,9 @@ public class FirstApp {
 
     public static void main (String[] args){
         System.out.println("Hello!");
-        
+        System.out.println("Hello1!");
+        System.out.println("Hello2!");
+        System.out.println("Hello3!");
+           
     }
 }
